8ec03c700f63b2b9d87f3c3528111439eff74127
Arto <arto.khyan@gmail.com>
2022-06-12 17:39:16 +0400

    Add ConcatMap...

---
+++ src/com/company/ConcatMapExample.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.TestScheduler;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class ConcatMapExample {
+
+    public static void main(String[] args) {
+
+        String strings []= {"a", "b", "c", "d", "e", "f"};
+        final List<String> items = new ArrayList<>();
+        items.addAll(Arrays.asList(strings));
+
+        final TestScheduler scheduler = new TestScheduler();
+
+        Observable.fromIterable(items)
+                .concatMap( s -> {
+                    final int delay = new Random().nextInt(10);
+                    return Observable.just(s + "x")
+                            .delay(delay, TimeUnit.SECONDS, scheduler);
+                })
+                //.toList().
+                .doOnNext(System.out::println)
+                .subscribe();
+
+        scheduler.advanceTimeBy(1, TimeUnit.MINUTES);
+    }
+
+//Технически данный оператор выдает похожий с FlatMap() результат, меняется только последовательность эмиттируемых данных.
+// ConcatMap() поддерживает порядок эмиссии данных и ожидает исполнения текущего Observable. Поэтому лучше использовать
+// его когда необходимо обеспечить порядок выполнения задач. При этом нужно помнить, что выполнение ConcatMap()
+// занимает больше времени чем FlatMap().
+
+}
