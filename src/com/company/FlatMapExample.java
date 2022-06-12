b65ca57df65c8ab5faffdee296ce7a59c6f875fb
Arto <arto.khyan@gmail.com>
2022-06-12 15:56:21 +0400

    Fix

--- src/com/company/FlatMapExample.java
+++ src/com/company/FlatMapExample.java
@@ -1,24 +1,36 @@
 package com.company;
 
 import io.reactivex.Observable;
+import io.reactivex.functions.Function;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class FlatMapExample {
     public static void main(String[] args) {
-        List<String> urls = new ArrayList<String>();
+        List<String> urls = new ArrayList<>();
         urls.add("url1");
         urls.add("url2");
         urls.add("url3");
 
         Observable.just(urls)
-                .flatMap(new Func1<List<String>, Observable<String>>() {
+                .flatMap(new Function<List<String>, Observable<String>>() {
                     @Override
-                    public Observable<String> call(List<String> urls) {
-                        return Observable.from(urls);
+                    public Observable<String> apply(List<String> strings) throws Exception {
+                        return Observable.fromIterable(strings);
                     }
                 })
                 .subscribe(url -> System.out.println(url));
+
+//Old realisation
+//        Observable.just(urls)
+//                .flatMap(new Func1<List<String>, Observable<String>>() {
+//                    @Override
+//                    public Observable<String> call(List<String> urls) {
+//                        return Observable.from(urls);
+//                    }
+//                })
+//                .subscribe(url -> System.out.println(url));
     }
 //FlatMap() принимает на вход данные, излучаемые одним Observable , и возвращает данные, излучаемые другим Observable,
 // подменяя таким образом один Observable на другой.
