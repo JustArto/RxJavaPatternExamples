76f7b6d34bc61f8456e8cf9f3c4962a190349b40
Arto <arto.khyan@gmail.com>
2022-06-18 23:55:09 +0400

    Main getClass

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Object o1 = String.valueOf(1);
+        Object o2 = 123 + "T";
+        System.out.println(o1.getClass().equals(o2.getClass()));
+        //o1.getClass() == o2.getClass();
+
+        Class s = int.class;
+        String name = s.getName();
+        System.out.println(name);
+
+        Class clazz = "123".getClass();
+        System.out.println(clazz);
+
+        Class[] interfaces = List.class.getInterfaces();
+        System.out.println(interfaces.getClass());
+
+        Method[] methods = List.class.getMethods();
+        System.out.println(Arrays.stream(methods).toList());
     }
 }
